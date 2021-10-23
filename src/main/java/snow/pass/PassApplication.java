e65d3e41a7a3b05d13ffd3e5e96845f3eda26040
mrhaffner <mrhaffner13@gmail.com>
2021-10-23 17:38:06 -0500

    feat: allow CORS

--- src/main/java/snow/pass/PassApplication.java
+++ src/main/java/snow/pass/PassApplication.java
@@ -2,6 +2,9 @@ package snow.pass;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 
 @SpringBootApplication
@@ -9,6 +12,16 @@ public class PassApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(PassApplication.class, args);
+	}
+
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/**");
+			}
+		};
 	}
 
 }
