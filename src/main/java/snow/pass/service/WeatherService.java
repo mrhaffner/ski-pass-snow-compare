ebce0abb148fe7dfb15f907f5e57a7e0adfdaba0
mrhaffner <mrhaffner13@gmail.com>
2021-10-31 19:21:14 -0500

    refactor: app now uses System.getenv instead of dotenv to work with heroku

--- src/main/java/snow/pass/service/WeatherService.java
+++ src/main/java/snow/pass/service/WeatherService.java
@@ -1,21 +1,32 @@
 package snow.pass.service;
 
 import org.springframework.stereotype.Service;
-import io.github.cdimascio.dotenv.Dotenv;
+// import io.github.cdimascio.dotenv.Dotenv;
 import snow.pass.model.WeatherData;
 
 import org.springframework.web.client.RestTemplate;
 
 
+// @Service
+// public class WeatherService {
+//     private Dotenv dotenv = Dotenv.load();
+
+//     private final RestTemplate restTemplate = new RestTemplate();
+
+//     public WeatherData getWeather(double longitude, double latitude) {
+//         String lat = Double.toString(latitude);
+//         String lon = Double.toString(longitude);
+//         return this.restTemplate.getForObject("https://api.weatherbit.io/v2.0/forecast/daily?&lat={latitude}&lon={longitude}&key=" + dotenv.get("API_KEY"), WeatherData.class, lat, lon);
+//     }
+// }
+
 @Service
 public class WeatherService {
-    private Dotenv dotenv = Dotenv.load();
-
     private final RestTemplate restTemplate = new RestTemplate();
 
     public WeatherData getWeather(double longitude, double latitude) {
         String lat = Double.toString(latitude);
         String lon = Double.toString(longitude);
-        return this.restTemplate.getForObject("https://api.weatherbit.io/v2.0/forecast/daily?&lat={latitude}&lon={longitude}&key=" + dotenv.get("API_KEY"), WeatherData.class, lat, lon);
+        return this.restTemplate.getForObject("https://api.weatherbit.io/v2.0/forecast/daily?&lat={latitude}&lon={longitude}&key=" + System.getenv("API_KEY"), WeatherData.class, lat, lon);
     }
 }
